0fc451ed589e04bddf0bed98b6b503d7d535305e
Andre Ferreira <andre.ferreira@mercatustecnologia.com.br>
2019-12-28 15:56:01 -0300

    término do projeto

--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -1,19 +1,33 @@
-﻿using BaltaStore.Domain.StoreContext.Handlers;
+﻿using System.IO;
+using BaltaStore.Domain.StoreContext.Handlers;
 using BaltaStore.Domain.StoreContext.Repositories;
 using BaltaStore.Domain.StoreContext.Service;
 using BaltaStore.Infra.StoreContext.DataContext;
 using BaltaStore.Infra.StoreContext.Repositories;
 using BaltaStore.Infra.StoreContext.Services;
+using BaltaStore.Shared;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BaltaStore.Api
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            // necessita configurar no azure
+            // services.AddApplicationInsightsTelemetry(Configuration);
+
             services.AddMvc();
             services.AddResponseCompression();
 
@@ -21,6 +35,13 @@ namespace BaltaStore.Api
             services.AddTransient<ICustomerRepository, CustomerRepository>();
             services.AddTransient<IEmailService, EmailService>();
             services.AddTransient<CustomerHandler>();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Info { Title = "Balta Store", Version = "v1" });
+            });
+
+            Settings.ConnectionString = $"{Configuration["connectionString"]}";
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -30,6 +51,15 @@ namespace BaltaStore.Api
 
             app.UseMvc();
             app.UseResponseCompression();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "Balta Store - V1");
+            });
+
+            // é necessário se cadastrar no site
+            // app.UseElmahIo("923f4c946cc1435cb0ec665d6e7370b7", new Guid("e42a9995-df89-4d91-a625-ecc57d124004"));
         }
     }
 }
